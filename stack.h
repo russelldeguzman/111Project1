5b1389da2998b11253ad2885c6bc7f337be2abd4
russelldeguzman <russelldeguzman@ucla.edu>
2015-09-27 20:26:14 -0700

    Stack -- First implementation
    UNTESTED implmentation of the stack.
    Will test & debug later.

--- stack.h
+++ stack.h
@@ -1,5 +1,18 @@
-//TODO: Implement Operator stack
 
+/*This implementation is of a GENERIC stack using void 
+pointers to access memory regardless of the data type */
 
+typedef struct{
+	void *data; 
+	int dataSize; 
+	int length;
+	int allocLength;
+} stack;
 
-//TODO: Implement Command stack
+void newStack(stack *st, int dataSize); //create stack
+void destroyStack(stack *st); //free stack
+bool StackisEmpty(const stack *st); //isempty
+void pushStack(stack *st, const void* add); //push
+void popStack(stack *st, void* top); //pop
+void topStack(stack *st, void* top); //view top element (doesnt affect stack)
+
