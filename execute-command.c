b59945278850ea24a17d964079c472db6f0bfe99
Russell Agaton Deguzman <deguzman@lnxsrv02.seas.ucla.edu>
2015-10-15 19:57:39 -0700

    Started execute-command.c.

--- execute-command.c
+++ execute-command.c
@@ -4,7 +4,9 @@
 #include "command-internals.h"
 
 #include <error.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
@@ -15,10 +17,45 @@ command_status (command_t c)
 }
 
 void
-execute_command (command_t c, int time_travel)
+execute_command (command_t c, int time_travel) //I think time_travel is implemented in 1c, won't touch for now
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+	if(c->type == AND_COMMAND){
+		//do something
+	}
+	else if(c->type == SEQUENCE_COMMAND){
+		//do something
+	}
+	else if(c->type == OR_COMMAND){
+		// do something
+	}
+	else if(c->type == PIPE_COMMAND){
+		//do something
+	}
+	else if(c->type == SIMPLE_COMMAND){
+		//simplest case: exectue the command.
+		pid_t pid = fork();
+		if(pid == 0){ //call execute in the child process
+		execvp(c->u.word[0],c->u.word);
+		//if execvp returns, there has been an error.
+		error(1,0, "Unknown command!");
+		}
+		else{
+			waitpid(pid, &(c->status), 0);
+			printf("%d\n",WEXITSTATUS(c->status)); 
+		}
+	}
+	else if(c->type == SUBSHELL_COMMAND){
+		//execute the subshell command
+		pid_t pid = fork();
+		if(pid == 0){
+			execute_command(c->u.subshell_command,time_travel);
+		}
+		else{
+			waitpid(pid, &(c->status),0);
+			printf("%d\n",WEXITSTATUS(c->status));
+		}
+	}
+	else{
+		error(1,0, "Invalid command type!");
+	}
 }
